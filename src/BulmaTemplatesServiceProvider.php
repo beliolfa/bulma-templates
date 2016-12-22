def4336cd20a6e00e19bf086d77c13f274d43388
disitec <fjmartinez@disitec.es>
2016-12-22 17:56:28 +0100

    remove controller

--- src/BulmaTemplatesServiceProvider.php
+++ src/BulmaTemplatesServiceProvider.php
@@ -14,10 +14,6 @@ class BulmaTemplatesServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadViewsFrom(__DIR__.'/../views', 'bulma-templates');
-
-        $this->publishes([
-            __DIR__.'/../templates/scaffold/controller' => resource_path('infyom/infyom-generator-templates/scaffold/controller')
-        ], 'simplified-controller');
     }
 
     /**
